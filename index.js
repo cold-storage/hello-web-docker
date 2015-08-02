76d2c53128c95b2060c6e72ccf9f3e921513c029
John Stein <john.stein@mandiant.com>
2015-08-02 07:12:51 -0400

    things

--- index.js
+++ index.js
@@ -2,16 +2,33 @@
 
 var http = require('http');
 var os = require("os");
+var fs = require('fs');
+var url = require('url');
 
 var hostname = os.hostname();
 var port = process.argv[2] || process.env.PORT || 1337;
-var msg = '<h1>http://' + hostname + ':' + port + ' says hello!</h1>';
+var thing = process.argv[3] || process.env.THING || 'thing1';
 
-http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-  });
-  res.end(msg);
+// if we got a thing but no port.
+if (!parseInt(port, 10)) {
+  thing = port;
+  port = 1337;
+}
+
+var msg = '<h3>' + thing + ' says, "hello", from ' + hostname + ':' +
+  port + '</h3><div><img width="300" src="' + thing + '.png"></img></div>';
+
+http.createServer(function(req, res){
+  var request = url.parse(req.url, true);
+  var action = request.pathname;
+  if (action === '/thing1.png' || action === '/thing2.png') {
+     var img = fs.readFileSync('./'+thing+'.png');
+     res.writeHead(200, {'Content-Type': 'image/gif' });
+     res.end(img, 'binary');
+  } else {
+     res.writeHead(200, {'Content-Type': 'text/html' });
+     res.end(msg);
+  }
 }).listen(port);
 
 console.log(msg);
